f823e66f0af7c2d8c8ddbfd127b2bc94789f48c7
xubo <450760938@qq.com>
2019-06-30 14:50:42 +0800

    format

--- 05-reocketmq/src/main/java/com/jincou/rocketmq/jms/JmsConfig.java
+++ 05-reocketmq/src/main/java/com/jincou/rocketmq/jms/JmsConfig.java
@@ -11,7 +11,7 @@ public class JmsConfig {
     /**
      * Name Server 地址，因为是集群部署 所以有多个用 分号 隔开
      */
-    public static final String NAME_SERVER = "47.99.203.55:9876;47.99.203.55:9877";
+    public static final String NAME_SERVER = "IP:9876;IP:9877";
 
     /**
      * 主题名称 主题一般是服务器设置好 而不能在代码里去新建topic（ 如果没有创建好，生产者往该主题发送消息 会报找不到topic错误）
